8983601f1425085e34e3cd01441da76ea4baa98e
Sonal <sonal020394@gmail.com>
2020-11-10 22:15:35 +0530

    upload file ti git

---
+++ src/test/java/AirAsia/AirAsiaLearning/AirAsiaLoginTest.java
@@ -0,0 +1,29 @@
+package AirAsia.AirAsiaLearning;
+
+import org.testng.annotations.Test;
+
+public class AirAsiaLoginTest {
+
+	@Test
+	public void loginid() {
+		
+		System.out.println("Maven");
+		
+	}
+	
+	@Test
+	public void password() {
+		System.out.println("TestNG");
+	}
+	
+	@Test
+	public void Submit() {
+		System.out.println("Selenium");
+		
+	}
+	
+	
+}
+
+
+
---
+++ src/test/java/AirAsia/AirAsiaLearning/AirAsiaRegistrationTest.java
@@ -0,0 +1,21 @@
+package AirAsia.AirAsiaLearning;
+
+import org.testng.annotations.Test;
+
+public class AirAsiaRegistrationTest {
+	
+	@Test
+	public void Reg_Details() {
+		
+	}
+	
+	@Test
+	public void Reg_Contact() {
+		
+	}
+
+	@Test
+	public void Reg_Address() {
+		
+	}
+}
